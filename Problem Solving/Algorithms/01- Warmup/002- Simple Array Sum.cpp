3c9c5d90e7ea52a1516373dbc277b5302b089346
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-03-06 22:50:58 +0500

    Add simple array sum

---
+++ Problem Solving/Algorithms/01- Warmup/002- Simple Array Sum.cpp
@@ -0,0 +1,11 @@
+// Problem: https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+int simpleArraySum(vector<int> ar)
+{
+    int sum = 0;
+    for (int i = 0; i < ar.size(); i++)
+    {
+        sum += ar[i];
+    }
+    return sum;
+}
