4f09b462de0d552b3732649f4658e661f658404a
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-03-08 09:13:44 +0500

    Add birthday cake candles

---
+++ Problem Solving/Algorithms/01- Warmup/009- Birthday Cake Candles.cpp
@@ -0,0 +1,6 @@
+Problem: https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+int birthdayCakeCandles(vector<int> candles)
+{
+    return count(candles.begin(), candles.end(), *max_element(candles.begin(), candles.end()));
+}
