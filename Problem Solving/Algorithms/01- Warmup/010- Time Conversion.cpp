e6e732f29547bee3b4f72d4ba655099dd90c3c3f
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-03-08 09:14:04 +0500

    Add time conversion

---
+++ Problem Solving/Algorithms/01- Warmup/010- Time Conversion.cpp
@@ -0,0 +1,21 @@
+Problem: https://www.hackerrank.com/challenges/time-conversion/problem
+
+string timeConversion(string s) {
+    char ampm = tolower(s[s.length() - 2]);
+    string time = s.substr(0, 8);
+    string hr = s.substr(0, 2);
+
+    if (ampm == 'p' && hr != "12")
+    {
+        hr = to_string((stoi(hr) + 12));
+        time.replace(0, 2, hr);
+    }
+    else if (ampm == 'a' && hr == "12")
+    {
+        hr = "00";
+        time.replace(0, 2, hr);
+    }
+
+    //if time is a.m. or 12 p.m, the time will be returned as is
+    return time;
+}
