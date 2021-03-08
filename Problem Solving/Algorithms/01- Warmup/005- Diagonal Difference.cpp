8998f3245f233ad3769a7ced6ab564e0bb7cc1eb
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-03-08 09:06:54 +0500

    Add diagonal difference

---
+++ Problem Solving/Algorithms/01- Warmup/005- Diagonal Difference.cpp
@@ -0,0 +1,16 @@
+//Problem: https://www.hackerrank.com/challenges/diagonal-difference/problem
+
+int diagonalDifference(vector<vector<int>> arr)
+{
+    int sum, pri = 0, sec = 0, rows = arr.size();
+
+    for (int i = 0, j = rows - 1; i < rows; i++, j--)
+    {
+        pri += arr[i][i];
+        sec += arr[i][j];
+    }
+    sum = pri - sec;
+    if (sum < 0)
+        sum = -sum;
+    return sum;
+}
