92370523a6d64b70af8d94171a5fb7cb1e9262f2
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-03-08 09:07:30 +0500

    Add plus minus

---
+++ Problem Solving/Algorithms/01- Warmup/006- Plus Minus.cpp
@@ -0,0 +1,21 @@
+Problem : https://www.hackerrank.com/challenges/plus-minus/problem
+
+void plusMinus(vector<int> arr)
+{
+    float positives = 0, negatives = 0, zeroes = 0;
+    int size = arr.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > 0)
+            positives += 1;
+        else if (arr[i] < 0)
+            negatives += 1;
+        else
+            zeroes += 1;
+    }
+
+    cout << setprecision(6) << positives / size << endl;
+    cout << negatives / size << endl;
+    cout << zeroes / size << endl;
+}
