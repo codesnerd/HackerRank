003c463f7546ea8d9b4422ee596cf6ab1340b9e7
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-03-12 00:03:50 +0500

    Add grading students

--- Problem Solving/Algorithms/02- Implementation/01- Grading Students.cpp
+++ Problem Solving/Algorithms/02- Implementation/01- Grading Students.cpp
@@ -1,7 +1,9 @@
-Problem: https://www.hackerrank.com/challenges/grading/problem
+//Problem: https:www.hackerrank.com/challenges/grading/problem
 
-vector<int> gradingStudents(vector<int> grades) {
-    for (int i = 0; i < grades.size(); i++) {
+vector<int> gradingStudents(vector<int> grades)
+{
+    for (int i = 0; i < grades.size(); i++)
+    {
         if (grades[i] >= 38 && grades[i] < 100 && grades[i] % 5 >= 3)
             grades[i] += 5 - grades[i] % 5;
     }
