d7ae290603b4d7aaa5599640718f1353589ddd57
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-03-12 00:00:00 +0500

    Add revising select query

--- Problem Solving/Algorithms/02- Implementation/01- Grading Students.cpp
+++ Problem Solving/Algorithms/02- Implementation/01- Grading Students.cpp
@@ -1,12 +1,9 @@
 Problem: https://www.hackerrank.com/challenges/grading/problem
 
 vector<int> gradingStudents(vector<int> grades) {
-    for (int i = 0; i < grades.size(); i++)
-    {
-        int nextMultiple = (((grades[i] / 5) + 1) * 5);
-        int difference = nextMultiple - grades[i];
-        if (grades[i] < 100 && grades[i] >= 38 && difference < 3)
-            grades[i] = nextMultiple;
+    for (int i = 0; i < grades.size(); i++) {
+        if (grades[i] >= 38 && grades[i] < 100 && grades[i] % 5 >= 3)
+            grades[i] += 5 - grades[i] % 5;
     }
     return grades;
 }
