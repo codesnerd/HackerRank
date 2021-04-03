e31e8faed034040e559493d26224d0d56d3aee68
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-04-04 04:37:19 +0500

    Update array ds

--- Problem Solving/Data Structures/01- Arrays/01- Arrays - DS.cpp
+++ Problem Solving/Data Structures/01- Arrays/01- Arrays - DS.cpp
@@ -1,10 +1,24 @@
 //Problem: https://www.hackerrank.com/challenges/arrays-ds/problem
 
 vector<int> reverseArray(vector<int> a) {
+
     vector <int> rev;
-    for (int i = a.size() - 1; i >= 0; i--) {
+    for (int i = a.size() - 1; i >= 0; i--)
         rev.push_back(a[i]);
-    }
 
     return rev;
+}
+
+//OR:
+
+vector<int> reverseArray(vector<int> a) {
+    int size = a.size();
+    int temp = 0;
+    for (int i = 0; i < size / 2; i++) {
+        temp = a[i];
+        a[i] = a[size - i - 1];
+        a[size - i - 1] = temp;
+    }
+
+    return a;
 }
