a61e25c1d9082b3592bd8b80a99d78a6e3c123aa
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-05-09 23:48:52 +0500

    Add sales by match

---
+++ Interview Preparation Kit/01- Warmup Challenges/01- Sales by Match.cpp
@@ -0,0 +1,16 @@
+// Problem: https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+int sockMerchant(int n, vector<int> ar) {
+    const int size = 101; // given 1 <= ar[i] <= 100
+    int pairs[size] = {};
+    int count = 0;
+
+    for (int i = 0; i < ar.size(); i++)
+        pairs[ar[i]]++;
+
+    for (int i = 0; i < size; i++)
+        if (pairs[i] != 0 && pairs[i] != 1)
+            count += pairs[i] / 2;
+
+    return count;
+}
