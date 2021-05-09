2be5aca32cbf5851feb77daab232960a6c7e5c02
Aamna <70039999+codesnerd@users.noreply.github.com>
2021-05-09 23:59:53 +0500

    Update 02 - 2D Array - DS.cpp

--- Problem Solving/Data Structures/01- Arrays/02 - 2D Array - DS.cpp
+++ Problem Solving/Data Structures/01- Arrays/02 - 2D Array - DS.cpp
@@ -1,13 +1,13 @@
 //Problem: https://www.hackerrank.com/challenges/2d-array/problem
 
 int hourglassSum(vector<vector<int>> arr) {
-    int max = -63; // given condition: -9 <= arr[i][j] <= 9
+    int max = -63, sum = 0; // given condition: -9 <= arr[i][j] <= 9
     int to_traverse = arr.size() - 2;
 
     int i = 0, j = 0;
     while (i < to_traverse)
     {
-        int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+        sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
             arr[i + 1][j + 1] +
             arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
 
