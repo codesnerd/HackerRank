b5f5d0e8ba70f29ab1480170a45da9a9a15638b7
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-05-14 02:13:54 +0500

    Add weather observation station 5

---
+++ SQL/01- Basic Select/10- Weather Observation Station 5.sql
@@ -0,0 +1,11 @@
+-- Problem: https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+(SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY) ASC, CITY ASC
+LIMIT 1)
+UNION
+(SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY) DESC, CITY ASC
+LIMIT 1);
