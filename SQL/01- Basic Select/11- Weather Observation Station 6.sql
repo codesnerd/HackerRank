47b2c02bb37e796e97c0a519aae43b250bf9b21a
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-05-15 03:09:00 +0500

    Add weather observation station 6

---
+++ SQL/01- Basic Select/11- Weather Observation Station 6.sql
@@ -0,0 +1,16 @@
+-- Problem: https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE LOWER(SUBSTR(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u');
+
+/*
+* Not Working:
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE LOWER(CITY) LIKE a%
+OR LOWER(CITY) LIKE e%
+OR LOWER(CITY) LIKE i%
+OR LOWER(CITY) LIKE o%
+OR LOWER(CITY) LIKE u%;
+*/
