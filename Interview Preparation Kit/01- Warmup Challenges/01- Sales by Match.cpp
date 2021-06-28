c262b7f03932843c6b501035ff3313dfe2f6bfcf
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-06-28 08:41:46 +0500

    Add set solution

--- Interview Preparation Kit/01- Warmup Challenges/01- Sales by Match.cpp
+++ Interview Preparation Kit/01- Warmup Challenges/01- Sales by Match.cpp
@@ -1,5 +1,7 @@
 // Problem: https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 
+//* Array based Solution:
+
 int sockMerchant(int n, vector<int> ar)
 {
     const int size = 101; // given 1 <= ar[i] <= 100
@@ -14,4 +16,25 @@ int sockMerchant(int n, vector<int> ar)
             count += pairs[i] / 2;
 
     return count;
+}
+
+//* Set based Solution:
+
+int sockMerchant(int n, vector<int> ar)
+{
+    unordered_set<int> socks;
+    int matched = 0;
+
+    for (int i = 0; i < ar.size(); i++)
+    {
+        if (socks.find(ar[i]) != socks.end()) //sock is already there
+        {
+            socks.erase(ar[i]);
+            matched++;
+        }
+        else
+            socks.insert(ar[i]);
+    }
+
+    return matched;
 }
