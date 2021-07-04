c0035bc6703503368c138a7fa4cbf1e5dfeba9cb
Aamna <70039999+codesnerd@users.noreply.github.com>
2021-07-04 21:18:19 +0500

    Update 005- Diagonal Difference.cpp

--- Problem Solving/Algorithms/01- Warmup/005- Diagonal Difference.cpp
+++ Problem Solving/Algorithms/01- Warmup/005- Diagonal Difference.cpp
@@ -1,16 +1,13 @@
 // Problem: https://www.hackerrank.com/challenges/diagonal-difference/problem
 
-int diagonalDifference(vector<vector<int>> arr)
-{
-    int sum, pri = 0, sec = 0, rows = arr.size();
-
-    for (int i = 0, j = rows - 1; i < rows; i++, j--)
+int diagonalDifference(vector<vector<int>> arr) {
+    int pri = 0, sec = 0, rows = arr.size();
+    
+    for (int i = 0, j = rows - 1; i < arr.size(); i++, j--)
     {
         pri += arr[i][i];
         sec += arr[i][j];
     }
-    sum = pri - sec;
-    if (sum < 0)
-        sum = -sum;
-    return sum;
+    
+    return abs(pri - sec);
 }
