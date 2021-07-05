b3dfd54cac70a7ce3f5fd3435aa0ec4625b56fa8
Aamna <70039999+codesnerd@users.noreply.github.com>
2021-07-05 09:42:35 +0500

    Add optimized 008- Mini-Max

--- Problem Solving/Algorithms/01- Warmup/008- Mini-Max.cpp
+++ Problem Solving/Algorithms/01- Warmup/008- Mini-Max.cpp
@@ -1,10 +1,12 @@
 // Problem: https://www.hackerrank.com/challenges/mini-max-sum/problem
 
+//* O(nlogn)
+
 void miniMaxSum(vector<int> arr)
 {
     long long int sum = 0, min = 0, max = 0;
     int size = arr.size();
-    sort(arr.begin(), arr.end());
+    sort(arr.begin(), arr.end()); // Quick Sort: O(nlogn)
 
     for (int i = 0; i < size; i++)
         sum += arr[i];
@@ -13,4 +15,22 @@ void miniMaxSum(vector<int> arr)
     max = sum - arr[0];
 
     cout << min << " " << max;
+}
+
+// O(n)
+
+void miniMaxSum(vector<int> arr) {
+    long int min = arr[0], max = min, sum = min;
+    
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] < min)
+            min = arr[i];
+            
+        if (arr[i] > max)
+            max = arr[i];
+            
+        sum += arr[i];
+    }
+    
+    cout << sum - max << " " << sum - min;
 }
