c7a56fcaf38e673d06e716abb63499680ddf23ac
Aamna <70039999+codesnerd@users.noreply.github.com>
2021-07-05 13:04:47 +0500

    Add optimized 009- Birthday Cake Candles

--- Problem Solving/Algorithms/01- Warmup/009- Birthday Cake Candles.cpp
+++ Problem Solving/Algorithms/01- Warmup/009- Birthday Cake Candles.cpp
@@ -4,3 +4,21 @@ int birthdayCakeCandles(vector<int> candles)
 {
     return count(candles.begin(), candles.end(), *max_element(candles.begin(), candles.end()));
 }
+
+//* O(n)
+
+int birthdayCakeCandles(vector<int> candles) {
+    int max = candles[0], count = 0;
+    
+    for (int i = 0; i < candles.size(); i++) {
+         if (candles[i] > max)
+            max = candles[i];
+    }
+    
+    for (int i = 0; i < candles.size(); i++) {
+        if (candles[i] == max)
+            count++;
+    }
+    
+    return count;
+}
