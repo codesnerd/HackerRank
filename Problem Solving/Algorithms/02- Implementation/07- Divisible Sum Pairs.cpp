860ea6f6c45abbe2e6771724774305e9fe595379
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-08-04 20:42:12 +0500

    Add optimized solution

--- Problem Solving/Algorithms/02- Implementation/07- Divisible Sum Pairs.cpp
+++ Problem Solving/Algorithms/02- Implementation/07- Divisible Sum Pairs.cpp
@@ -1,6 +1,6 @@
 // Problem: https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 
-// O(n²)
+//* O(n²)
 int divisibleSumPairs(int n, int k, vector<int> ar)
 {
     int count = 0;
@@ -8,6 +8,29 @@ int divisibleSumPairs(int n, int k, vector<int> ar)
         for (int j = i + 1; j < n; j++)
             if ((ar[i] + ar[j]) % k == 0)
                 count++;
+
+    return count;
+}
+
+//* O(n) Hashmap Solution:
+int divisibleSumPairs(int n, int k, vector<int> ar)
+{
+    int count = 0;
+    unordered_map<int, int> rfmap; //remainder frequency map
+
+    for (int i = 0; i < ar.size(); i++)
+    {
+        int rem = ar[i] % k;
+        int complement = k - rem == k ? 0 : k - rem; //if complement is a factor of k, then store 0 instead of complement
+
+        if (rfmap.find(complement) != rfmap.end()) //complement is present in map
+            count += rfmap[complement];
+
+        if (rfmap.find(rem) == rfmap.end())
+            rfmap.insert({rem, 1});
+        else
+            rfmap[rem]++;
+    }
 
     return count;
 }
